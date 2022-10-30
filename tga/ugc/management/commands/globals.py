d35832e6efaa56f15f09e6cbc667f2bb38b46dae
komiljon <komiljon.zoom@gmail.com>
2022-10-30 09:39:01 +0500

    add project

---
+++ tga/ugc/management/commands/globals.py
@@ -0,0 +1,185 @@
+WELCOME_TEXT = "Xush kelibsiz / Добро пожаловать!"
+CHOOSE_LANG = "Tilni tanlang / Выберите язык!"
+BTN_LANG_UZ = "O'zbek tili"
+BTN_LANG_RU = "Русский язык"
+BTN_LANG_EN = "English"
+STATES = {
+    "reg": 1,
+    "menu": 2,
+    "settings":3,
+    "feedback":4,
+    "admin":5
+}
+LANGUAGE_CODE = {
+    1: "uz",
+    2: "ru",
+    3: "en",
+}
+TEXT_LANG_WARNING = "Iltimos tillardan birini tanlang!\nПожалуйста, выберите один из языков!"
+TEXT_ENTER_FIRST_NAME = {
+    1: "👤 Iltimos ismingizni kiriting:",
+    2: "👤 Пожалуйста, введите ваше имя:",
+    3: "👤 Please enter your first name: "
+}
+TEXT_ENTER_LAST_NAME = {
+    1: "😊 Iltimos familiyangizni kiriting:",
+    2: "😊 Пожалуйста, введите свою фамилию:",
+    3: "😊 Please enter your last name",
+}
+SENDED_TO_ADMIN = {
+    1: "Sizning buyurtmangiz qabul qilindi! Tez orada aloqaga chiqamiz!",
+    2: "Ваш заказ принят! Мы свяжемся с вами в ближайшее время!",
+    3: "Your order has been accepted! We will get in touch soon!"
+
+}
+BTN_SEND_CONTACT = {
+    1: "📞 Yuborish",
+    2: "📞 Отправить",
+    3: "📞 Send",
+}
+BTN_NEWS = {
+    1:"📢 Yangiliklar",
+    2:"📢 Новости",
+    3:"📢 News"
+}
+SEND_LOCATION = {
+    1: "📍 Lokatsiya jo'nating",
+    2: "📍 Отправить местоположение",
+    3: "📍 Send location",
+}
+TEXT_ENTER_CONTACT = {
+    1: "Iltimos telefon raqamingizni yozing yoki <b>📞 Yuborish</b> tugmasini bosing:",
+    2: "Введите свой номер телефона или нажмите кнопку <b>📞 Отправить </b>:",
+    3: "Please enter your phone number or  click the <b> 📞 Send </b> button:",
+}
+TEXT_MAIN_MENU = {
+    1: "🏠 Asosiy menyu",
+    2: "🏠 Главное меню",
+    3: "🏠 Main menu"
+}
+BTN_ORDER = {
+    1:"🛒 Buyurtma qilish",
+    2:"🛒 Заказать",
+    3:"🛒 To order",
+}
+TEXT_ORDER = {
+    1: "Kategoriyalardan birini tanlang:",
+    2: "Выбери одну из категорий:",
+    3: "Select one of the categories",
+}
+BTN_KORZINKA = {
+    1: "🛍 Xarid qilish",
+    2: "🛍 Покупка товаров",
+    3: "🛍 Shopping",
+}
+BTN_MY_ORDERS = {
+    1:"🛍 Buyurtmalarim",
+    2:"🛍 Мои заказы",
+    3:"🛍 My orders",
+}
+BTN_ABOUT_US = {
+    1:"👨‍👩‍👦 Biz haqimizda",
+    2:"👨‍👩‍👦 O нас",
+    3:"👨‍👩‍👦 About us",
+}
+BTN_COMMENTS = {
+    1:"✍ Fikr bildirish",
+    2:"✍ Kомментарий",
+    3:"✍ Feedback",
+}
+BTN_SETTINGS = {
+    1:"⚙ Sozlamalar",
+    2:"⚙ Настройки",
+    3:"⚙ Settings",
+}
+TEXT_PRODUCT_PRICE = {
+    1: "Narx:",
+    2: "Цена:",
+    3: "Cost:",
+}
+TEXT_PRODUCT_DESC = {
+    1: "Tarkibi:",
+    2: "Состав:",
+    3: "Ingredients:"
+}
+TEXT_GIVE_FEEDBACK = {
+    1: "Fikr va takliflaringizni yozib qoldiring:",
+    2: "Запишите свои мысли и предложения:",
+    3: "Leave your comments and suggestions:",
+
+}
+BACK={
+    1:"Ortga",
+    2:"Назад",
+    3:"Back",
+}
+AT_KORZINKA = {
+    1: "Savatchada",
+    2: "В корзине",
+    3: "In the cart",
+}
+PAYMENT_TYPE_1 = {
+    1:"💴 naqd pul",
+    2: "💴 оплата наличными",
+    3: "💴 cash",
+}
+PAYMENT_TYPE_2 = {
+    1:"💳 karta",
+    2: "💳 карта",
+    3: "💳 card",
+}
+PAYMENT = {
+    1: "наличными",
+    2:"карта",
+}
+
+CLEAR_CART = {
+    1:"🗑 Savatchani bo'shatish",
+    2:"🗑 Очистить корзину",
+    3:"🗑 Empty the cart",
+
+}
+
+BUY = {
+    1:"📤 Buyurtma berish",
+    2:"📤 Заказать",
+    3:"📤 To order",
+}
+ALL = {
+    1: "Jami",
+    2: "Все",
+    3: "Amount",
+}
+
+ZAKAZ = {
+    1: "Buyurtma",
+    2: "Заказ",
+    3: "Order",
+}
+
+SUM = {
+    1: "so'm",
+    2: "сум",
+    3: "UZS",
+}
+
+ABOUT_COMPANY = {
+    1: "Biz <b>DelX Group</b>miz! "
+         "\n\nBirgalikda biznesingizni rivojlantiramiz va uni "
+         "zamonaviy uslubda ommaga taqdim qilamiz!\n\n"
+         "<b>BIZ SIZ HAQINGIZDA QAYG'URAMIZ!!!</b>",
+    2: "Мы <b>DelX Group</b>!"
+       "\n\nМы будем развивать ваш бизнес вместе"
+       "и представляем публике в современном стиле!\n\n"
+       "<b>МЫ ЗАБОТИМСЯ О ВАС !!!</b>",
+    3: "Мы <b>DelX Group</b>!"
+       "\n\nМы будем развивать ваш бизнес вместе"
+       "и представляем публике в современном стиле!\n\n"
+       "<b>МЫ ЗАБОТИМСЯ О ВАС !!!</b>",
+}
+
+NO_ZAKAZ = {
+    1: "Siz hali birorta buyurtma bermadingiz!",
+    2: "Вы еще не разместили ни одного заказа!",
+    3: "You haven't ordered anything yet!",
+}
